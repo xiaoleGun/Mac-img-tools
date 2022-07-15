82c25ca1fb167b9f504f9bbd790dc9cdc4e4db27
xiaoleGun <1592501605@qq.com>
2022-07-15 21:02:36 +0800

    initial commit
    Thanks @xiaoxindada
    
    Signed-off-by: xiaoleGun <1592501605@qq.com>

---
+++ README.md
@@ -0,0 +1,14 @@
+# Mac img Tools
+
+## 使用
+解包
+```shell
+sudo bash unpackimg.sh
+```
+打包
+```shell
+sudo bash makeimg.sh
+```
+## 感谢
+
+[xiaoxindada](https://github.com/xiaoxindada)
---
+++ unpackimg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Copyright (C) 2020 Xiaoxindada <2245062854@qq.com>
+
+rm -rf ./out
+mkdir ./out
+
+echo ""
+read -p "请输入要接包的分区(别带.img): " species
+
+echo "正在解压$species......."
+python3 ./bin/imgextractor.py ./$species'.img' ./out
